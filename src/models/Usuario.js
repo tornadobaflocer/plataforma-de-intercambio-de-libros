d089f3925449802b5c57d55bbaaf926efeed4202
tornadobaflocer <jhudsubi@hotmail.com>
2025-08-14 23:34:18 -0500

    feat: initial commit for src/models/Usuario.js

---
+++ src/models/Usuario.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contrasena: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
