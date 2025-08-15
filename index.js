cb19b94440bc4f8b4bbcecb616a36730dbb84bac
tornadobaflocer <jhudsubi@hotmail.com>
2025-08-14 23:34:15 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoURI = 'mongodb://localhost:27017/libros-intercambio';
+
+// Conectar a MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.error(err));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la plataforma de intercambio de libros');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
